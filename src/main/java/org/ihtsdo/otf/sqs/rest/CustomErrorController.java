b645077c78432ce949bccd5484e6740a732f550a
Darryn McGaw <dmcg@ihtsdo.org>
2023-09-20 15:33:13 +0100

    PIP-386 Use jakarta instead of javax for HttpServletRequest

--- src/main/java/org/ihtsdo/otf/sqs/rest/CustomErrorController.java
+++ src/main/java/org/ihtsdo/otf/sqs/rest/CustomErrorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/error")
